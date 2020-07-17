29f86173a279fc1a2dc7b9f242270a8a6c7f5fce
kouz <lastpuzly@gmail.com>
2020-07-17 16:39:06 +0900

    feat: 숫자 제출 기능 구현

--- src/main/resources/static/game.js
+++ src/main/resources/static/game.js
@@ -1,5 +1,7 @@
 function Game() {
   const $showTargetButton = document.querySelector("#show-target");
+  const $guessButton = document.querySelector("#guess-button");
+  const $guessInput = document.querySelector("#guess-input");
   let location;
 
   const initGame = () => {
@@ -19,8 +21,34 @@ function Game() {
     alert(target);
   }
 
+  const onGuessHandler = async (event) => {
+    event.preventDefault();
+    const gameId = location.split("/")[2];
+    const guessValue = $guessInput.value;
+    const request = {"guess": guessValue};
+    const response = await fetch(`/guesses/${gameId}`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(request)
+    });
+    const responseObject = await response.json();
+    if (responseObject.guessResultType === "MATCH") {
+      alert("정답!");
+    } else if (responseObject.ballCount === 0 && responseObject.strikeCount === 0) {
+      alert("낫싱");
+    } else {
+      alert(responseObject.strikeCount + " 스트라이크 " + responseObject.ballCount + " 볼");
+    }
+
+    console.log(responseObject);
+  }
+
   const initEventListener = () => {
-    $showTargetButton.addEventListener("click", onShowTargetHandler)
+    $showTargetButton.addEventListener("click", onShowTargetHandler);
+    $guessButton.addEventListener("click", onGuessHandler)
   }
 
   this.init = () => {
